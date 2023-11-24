641245841d86af8d538be343d74893f80a678e57
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-11-24 23:53:40 +0200

    Changed test

--- BreeZip/BreeZipTests.cs
+++ BreeZip/BreeZipTests.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Interfaces;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Interactions;
+using System;
 using System.Xml.Linq;
 
 namespace BreeZip
@@ -12,8 +14,10 @@ namespace BreeZip
         private const string AppiumUriString = "http://127.0.0.1:4723/wd/hub";
         private const string appIdentifier = @"3138AweZip.AweZip_ffd303wmbhcjt!App";
         private const string defaultLocation = @"D:\Pics\Svatba_11.11.2023";
+        private const string tempDirectory = @"C:\temp";
         private WindowsDriver<WindowsElement> driver;
         private AppiumOptions options;
+           
         
         [SetUp]
         public void OpenApp()
@@ -22,8 +26,19 @@ namespace BreeZip
             options.AddAdditionalCapability("app", appIdentifier);
             this.driver = new WindowsDriver<WindowsElement>(new Uri(AppiumUriString), options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            
 
+            if (Directory.Exists(tempDirectory))
+            {
+                Directory.Delete(tempDirectory, true);
+
+            }
+
+            Directory.CreateDirectory(tempDirectory);
+            Thread.Sleep(1000);
         }
+
+    
 
         
 
@@ -58,10 +73,45 @@ namespace BreeZip
             var buttonArchive = driver.FindElementByAccessibilityId("archiveBtn");
             buttonArchive.Click();
 
+            var popupWindow = driver.FindElementByName("Create a new archive - BreeZip");
+            popupWindow = driver.FindElementByName("Close");
+            popupWindow.Click();
 
-            Thread.Sleep(6000);
+            inputField.Clear();
+            inputField.SendKeys(defaultLocation + @"\" + randomName + ".zip");
+            inputField.SendKeys(Keys.Enter);
+ 
+            var buttonExtract = driver.FindElementByName("Extraction");
+            buttonExtract.Click();
 
-            Assert.That(randomName, Is.EqualTo(randomName));
+            var extractionPath = driver.FindElementByAccessibilityId("changeExtractPathBtn");
+            extractionPath.Click();
+
+            Thread.Sleep(3000);
+
+            var sideCursor = driver.FindElementByName("All locations");
+            sideCursor.Click();
+
+            var inputFieldNew = driver.FindElementByName("Address");
+            inputFieldNew.SendKeys(tempDirectory);
+            inputFieldNew.SendKeys(Keys.Enter);
+
+              
+
+            var extractButtonClick = driver.FindElementByAccessibilityId("extractBtn");
+            extractButtonClick.Click();
+
+            string originalFilePath = Path.Combine(defaultLocation, "IMG-410eff1c1c792e3f2f714d7e7191c2c1-V.jpg");
+            string extractedFilePath = Path.Combine(tempDirectory, "IMG-410eff1c1c792e3f2f714d7e7191c2c1-V.jpg");
+
+          
+            bool areFileNamesEqual = String.Equals(
+                Path.GetFileName(originalFilePath),
+                Path.GetFileName(extractedFilePath),
+                StringComparison.OrdinalIgnoreCase);
+
+           
+            Assert.IsTrue(areFileNamesEqual, "File names in the original and extracted directories should be the same.");
         }
     }
 }
